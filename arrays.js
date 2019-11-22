9d8de6f9e8cff65507062730af775bc13907ee4d
Anas K <mail.anascape@gmail.com>
2019-11-22 06:49:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
   const array = [1]
-  var element = ["foo"]
+  
   ["foo", ...array]
 
   }
